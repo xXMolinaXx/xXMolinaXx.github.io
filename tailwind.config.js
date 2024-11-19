35c1420cbe4908616412172b06b5ad28170d6766
kenny molina <kenny.molina.murillo@gmail.com>
2024-11-19 16:12:57 -0600

    add tailwind css part 2 of 2

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./**/*.html'], // Adjust paths as necessary
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
